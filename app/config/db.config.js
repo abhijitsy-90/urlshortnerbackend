c978bb368a95d573c6494885bbed194046cb7d61
abhijitsy-90 <abhijitsy20001@gmail.com>
2025-01-03 17:28:56 +0530

    serverdatabasecredentialupdate

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,11 @@
-
+ 
+  
+  
   module.exports = {
-    HOST:"localhost",
+    HOST:"139.84.136.174",
     USER:"postgres",
-    PASSWORD:"1500",
-    DB: "urlshortnar",
+    PASSWORD:"Investry@123",
+    DB: "urlshortner",
     PORT:5432,
     dialect: "postgres",
     pool: {
@@ -12,9 +14,7 @@
       acquire: 30000,
       idle: 10000
     }
-  };     
-  
-  
+  };   
     
        
   
